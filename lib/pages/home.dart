fc01e46cd1ab8cd226c08380f6e41c67ba5b9526
Katja <katja.korzneva@gmail.com>
2024-05-06 15:22:42 +0300

    DatabaseHelper: creating db, adding item, getting items

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meds_tracker/services/database_helper.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -23,6 +24,25 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
         centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          try {
+            // Await the future to get the result of the database insertion
+            // int id = await DatabaseHelper.createItem(
+            //     'vitamin c', '1 pill', '02.06.2024', 1);
+            // Once the future completes, the result can be used immediately
+            // List<Map<String, dynamic>> myData = [];
+            // final data = await DatabaseHelper.getItems(1);
+            // myData = data;
+            // print(myData);
+          } catch (e) {
+            // If an error occurs, it will be caught here
+            print('Failed to insert item: $e');
+          }
+        },
+        child: Icon(Icons.add),
+        tooltip: 'Add new medication', // Optional: Tooltip text on long press
       ),
     );
   }
---
+++ lib/services/database_helper.dart
@@ -0,0 +1,89 @@
+import 'package:sqflite/sqflite.dart' as sql;
+
+class DatabaseHelper {
+  static Future<sql.Database> db() async {
+    try {
+      return sql.openDatabase(
+        'medstracker.db',
+        version: 1,
+        onCreate: (sql.Database database, int version) async {
+          await createTables(database);
+        },
+      );
+    } catch (e) {
+      print("An error occurred while opening the database: $e");
+      throw Exception("Failed to open the database");
+    }
+  }
+
+  static Future<void> createTables(sql.Database database) async {
+    try {
+      // Create Medication table
+      //prescdeadline is stored as TEXT in ISO8601 format, although it's called date
+      await database.execute("""
+      CREATE TABLE Medication(
+        medicationID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        medname TEXT,
+        dosage TEXT,
+        prescdeadline TEXT,  
+        active INTEGER
+      )
+      """);
+
+      // Create Reminder table
+      // Time can be stored as TEXT in 'HH:MM' format or as ISO8601
+      await database.execute("""
+      CREATE TABLE Reminder(
+        reminderID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        medicationID INTEGER,
+        time TEXT,  
+        active INTEGER,
+        FOREIGN KEY(medicationID) REFERENCES Medication(medicationID)
+      )
+    """);
+
+      // Create MedicationTaken table
+      await database.execute("""
+      CREATE TABLE Medicationtaken(
+        medicationtakenID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        medicationID INTEGER,
+        time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  
+        FOREIGN KEY(medicationID) REFERENCES Medication(medicationID)
+      )
+    """);
+    } catch (e) {
+      print("An error occurred while creating tables: $e");
+    }
+  }
+
+  // Create new medication entry
+  static Future<int> createItem(String? medname, String? dosage,
+      String? prescdeadline, int? active) async {
+    try {
+      final db = await DatabaseHelper.db();
+      final data = {
+        'medname': medname,
+        'dosage': dosage,
+        'prescdeadline': prescdeadline,
+        'active': active
+      };
+      final id = await db.insert('Medication', data,
+          conflictAlgorithm: sql.ConflictAlgorithm.replace);
+      return id;
+    } catch (e) {
+      print("An error occurred while creating a new medication: $e");
+      throw Exception("Failed to create a new medication");
+    }
+  }
+
+  //Get items based on active status
+  static Future<List<Map<String, dynamic>>> getItems(int active) async {
+    try {
+      final db = await DatabaseHelper.db();
+      return db.query('Medication', where: "active = ?", whereArgs: [active]);
+    } catch (e) {
+      print("An error occurred while fetching items: $e");
+      throw Exception("Failed to fetch items");
+    }
+  }
+}
