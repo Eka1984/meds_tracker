1998b92d72e9f80062e94cc83131f81bc64db931
matildalalli <matilda.lalli@edu.lapinamk.fi>
2024-05-08 11:43:52 +0300

    Merging add medication and medication cards
    Merging add medication and medication cards together

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
 import 'new_medication.dart';
+import 'package:meds_tracker/services/database_helper.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  //List with data fetched from the database
+  List<Map<String, dynamic>> myData = [];
+
+  //Function refreshing myData variable and the list of meds
+  Future<void> _refreshData() async {
+    final data = await DatabaseHelper.getItems(1);
+    setState(() {
+      myData = data;
+    });
+  }
+
+  //Refreshing the list of meds on the first load of the page
+  @override
+  void initState() {
+    super.initState();
+    _refreshData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,29 +39,105 @@ class _HomePageState extends State<HomePage> {
           style: TextStyle(
             color: Theme.of(context).colorScheme.onPrimary,
             fontSize: 25,
-            //fontWeight: FontWeight.bold,
           ),
         ),
         centerTitle: true,
       ),
-      body: const Center(
-
-        // Center text
+      body: myData.isEmpty
+          ? Center(
         child: Text(
-          'No meds today',
-          style: TextStyle(fontSize: 25),
+          "No meds today",
+          style: Theme.of(context).textTheme.headline6?.copyWith(
+            color: Theme.of(context).colorScheme.primary,
+          ),
         ),
+      )
+          : ListView.builder(
+        shrinkWrap: true,
+        itemCount: myData.length,
+        itemBuilder: (context, index) {
+          return InkWell(
+            onTap: () {
+              // Navigate to the details page when the card is tapped
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => NewMedicationPage()),
+              ).then((_) {
+                // Refresh data when returning from NewMedicationPage
+                _refreshData();
+              });
+            },
+            child: Card(
+              color: Theme.of(context).colorScheme.surfaceVariant,
+              margin: const EdgeInsets.all(10),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ListTile(
+                    title: Text(myData[index]['medname']),
+                    subtitle: Text(myData[index]['prescdeadline']),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        PopupMenuButton<String>(
+                          onSelected: (String result) {
+                            if (result == 'delete') {
+                              // Handle delete action
+                            } else if (result == 'history') {
+                              // Handle history action
+                            }
+                          },
+                          itemBuilder: (BuildContext context) =>
+                          <PopupMenuEntry<String>>[
+                            const PopupMenuItem<String>(
+                              value: 'delete',
+                              child: Text('Delete'),
+                            ),
+                            const PopupMenuItem<String>(
+                              value: 'history',
+                              child: Text('History'),
+                            ),
+                          ],
+                          icon: Icon(Icons.more_vert),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Handle 'Take' action here
+                      },
+                      child: Text('Take'),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                        Theme.of(context).colorScheme.primary,
+                        foregroundColor:
+                        Theme.of(context).colorScheme.onPrimary,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
 
       // Button
       floatingActionButton: FloatingActionButton.large(
         onPressed: () {
           Navigator.push(
-              context,
-          MaterialPageRoute(builder: (context) => NewMedicationPage()),
-          );
+            context,
+            MaterialPageRoute(builder: (context) => NewMedicationPage()),
+          ).then((_) {
+            // Refresh data when returning from NewMedicationPage
+            _refreshData();
+          });
         },
-        child: const Icon( Icons.add_circle),
+        child: Icon(Icons.add_circle),
         backgroundColor: Theme.of(context).colorScheme.primaryContainer,
       ),
     );
--- lib/pages/new_medication.dart
+++ lib/pages/new_medication.dart
@@ -9,6 +9,21 @@ class NewMedicationPage extends StatefulWidget {
 }
 
 class _NewMedicationPageState extends State<NewMedicationPage> {
+  final TextEditingController _medNameController = TextEditingController();
+  final TextEditingController _dosageController = TextEditingController();
+  final TextEditingController _remindersController = TextEditingController();
+  final TextEditingController _prescDeadlineController =
+  TextEditingController();
+
+  @override
+  void dispose() {
+    _medNameController.dispose();
+    _dosageController.dispose();
+    _remindersController.dispose();
+    _prescDeadlineController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,137 +39,93 @@ class _NewMedicationPageState extends State<NewMedicationPage> {
         ),
         centerTitle: true,
       ),
-      body: NewMedication(),
-    );
-  }
-}
-
-class NewMedication extends StatefulWidget {
-  const NewMedication({Key? key}) : super(key: key);
-
-  @override
-  _NewMedicationState createState() => _NewMedicationState();
-}
-
-class _NewMedicationState extends State<NewMedication> {
-  final TextEditingController _medNameController = TextEditingController();
-  final TextEditingController _dosageController = TextEditingController();
-  final TextEditingController _remindersController = TextEditingController();
-  final TextEditingController _intervalController = TextEditingController();
-  final TextEditingController _prescDeadlineController =
-  TextEditingController();
-
-  @override
-  void dispose() {
-    _medNameController.dispose();
-    _dosageController.dispose();
-    _remindersController.dispose();
-    _intervalController.dispose();
-    _prescDeadlineController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextField(
-            controller: _medNameController,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Enter a medicine name',
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+              controller: _medNameController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter a medicine name',
+              ),
             ),
           ),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextField(
-            controller: _dosageController,
-            decoration: InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'Enter dosage',
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+              controller: _dosageController,
+              decoration: InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Enter dosage',
+              ),
             ),
           ),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextField(
-            controller: _remindersController,
-            decoration: InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'Enter reminders',
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+              controller: _remindersController,
+              decoration: InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Enter reminders',
+              ),
             ),
           ),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextField(
-            controller: _intervalController,
-            decoration: InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'Enter interval',
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+              controller: _prescDeadlineController,
+              decoration: InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Enter prescription deadline',
+              ),
             ),
           ),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextField(
-            controller: _prescDeadlineController,
-            decoration: InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'Enter prescription deadline',
-            ),
-          ),
-        ),
 
-        // Save and Cancel Buttons
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () async {
-                // Retrieve data from text fields
-                String medName = _medNameController.text;
-                String dosage = _dosageController.text;
-                String reminders = _remindersController.text;
-                String interval = _intervalController.text;
-                String prescDeadline = _prescDeadlineController.text;
+          // Save and Cancel Buttons
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () async {
+                  // Retrieve data from text fields
+                  String medName = _medNameController.text;
+                  String dosage = _dosageController.text;
+                  String reminders = _remindersController.text;
+                  String prescDeadline = _prescDeadlineController.text;
 
-                // Save data to the database
-                try {
-                  await DatabaseHelper.createItem(
-                    medName,
-                    dosage,
-                    prescDeadline,
-                    reminders,
-                    interval,
-                    1, // Assuming active status is 1 for new medications
-                  );
+                  // Save data to the database
+                  try {
+                    await DatabaseHelper.createItem(
+                      medName,
+                      dosage,
+                      prescDeadline,
+                      reminders,
+                      1, // Assuming active status is 1 for new medications
+                    );
 
-                  // Navigate back to the home screen
+                    // Navigate back to the home screen
+                    Navigator.pop(context);
+                  } catch (e) {
+                    // Handle error
+                    print("Error saving medication: $e");
+                    // Optionally show a snackbar or dialog to inform the user about the error
+                  }
+                },
+                child: Text('Save'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Navigate back to the home screen without saving
                   Navigator.pop(context);
-                } catch (e) {
-                  // Handle error
-                  print("Error saving medication: $e");
-                  // Optionally show a snackbar or dialog to inform the user about the error
-                }
-              },
-              child: Text('Save'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate back to the home screen without saving
-                Navigator.pop(context);
-              },
-              child: Text('Cancel'),
-            ),
-          ],
-        ),
-      ],
+                },
+                child: Text('Cancel'),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
-
