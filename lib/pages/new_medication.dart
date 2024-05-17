cd009c1104c1e999d81dd460922978d9cfa1f3a6
Katja <katja.korzneva@gmail.com>
2024-05-17 11:59:20 +0300

    color name fixed

--- lib/pages/new_medication.dart
+++ lib/pages/new_medication.dart
@@ -52,7 +52,7 @@ class _NewMedicationPageState extends State<NewMedicationPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
+      backgroundColor: Theme.of(context).colorScheme.surface,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.primary,
         title: Text(
